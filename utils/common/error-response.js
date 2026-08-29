7c9939671ee1855745dac86daf306d9cbdbf8f33
Faiz Ali Warsi <faizakhtar7869@gmail.com>
2026-08-29 20:24:05 +0530

    Remove duplicate root-level folders, keep src/ as source of truth

--- utils/common/error-response.js
+++
@@ -1,8 +0,0 @@
-const ErrorResponse = {
-    success: false,
-    message: 'Something went wrong.',
-    data: {},
-    error: {},
-};
-
-module.exports = ErrorResponse;
--- utils/index.js
+++
@@ -1,9 +0,0 @@
-const compareTime = require('./helpers/datetimehelpers');
-
-module.exports = {
-    AppError: require('./errors'),
-    SuccessResponse: require('./common'),   
-    ErrorResponse: require('./common'),
-    compareTime : require('./helpers/datetimehelpers'),
-    enums: require('./common')
-};
